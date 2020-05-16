805e368054f4ccdd1e8ad6453fcffb3c9805913c
Sahil Maurya <sahilmaurya.756412@gmail.com>
2020-05-16 19:34:08 +0530

    Made minor changes

--- Gestures.py
+++ Gestures.py
@@ -10,10 +10,10 @@ def perform_gesture(result,opponent,categ):
         return
 
     if result=='w':
-        print(df[df['categ']==categ].iloc[0]['win'].format(opponent.capitalize()))
+        print(df[df['categ']==categ].iloc[0]['lose'].format(opponent.capitalize()))
 
     elif result=='l':
-        print(df[df['categ']==categ].iloc[0]['lose'].format(opponent.capitalize()))
+        print(df[df['categ']==categ].iloc[0]['win'].format(opponent.capitalize()))
 
     else:
         print("""You and {} are rated the same level of {}.That's really interesting!
@@ -21,7 +21,7 @@ def perform_gesture(result,opponent,categ):
 
 
 def ester(result,opponent,categ):
-    if result=='w' and opponent=='pushpanshu' and categ=='size':
+    if result=='l' and opponent=='pushpanshu' and categ=='size':
         print("Thats why he do not want to marry.Confidence!")
         return 1
 
